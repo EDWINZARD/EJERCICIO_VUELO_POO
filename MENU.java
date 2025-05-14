36ab1f05952df566bad91f2a8f9e868b40726402
EDWINZARD <esermitanov@udistrital.edu.co>
2025-05-13 21:01:26 -0500

    inicio menu

---
+++ MENU.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MENU {
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+    boolean salir= false;
+    }
+
+}
