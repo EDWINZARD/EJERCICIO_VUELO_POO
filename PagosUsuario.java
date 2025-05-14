722a0225cb219df2ef7ed751602da02071a07d27
EDWINZARD <esermitanov@udistrital.edu.co>
2025-05-13 21:42:11 -0500

    ono la politzia

--- PagosUsuario.java
+++ PagosUsuario.java
@@ -1,14 +1,24 @@
 public class PagosUsuario {
-    //DatosPersonales
+    //Datos Personales
     public Nombres String;
     public Apellidos String;
     public Nacimiento int;
     public Sexo Stirng;
-    //DatosTickets
+    //Datos Tickets
     public numeroTickets int[];
     public numeroMaletas int;
     public Va_niños_pequeños boolean;
-
+    public Va_Mascotas boolean;
+    public Fecha_Vuelo_Ida int;
+    public Fecha_Vuelo_Regreso int;
+    public Precio_Ticket int[];
+    public Cuotas_Tickets boolean;
+    public Hora_Vuelo_Ida int;
+    public Hora_Vuelo_Regreso int;
+    //Pago Virtual
+    private Numero_Tarjeta int;
+    private Vencimiento_Tarjeta int[4];
+    private Clave_Tarjeta int[3];
 }
 
 
